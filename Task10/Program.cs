ff743fe98af6a922814cb75de32c2ffc129fe377
Norros <Andriyxa_77@mail.ru>
2023-02-17 18:39:33 +0300

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+//Методы
+
+bool CheckThreeDiget(int num)
+{
+    return num > 100 && num < 1000;
+}
+int CheckSecondDiget(int num)
+{
+    int firstDig = num / 10;
+    int secondDiget = firstDig % 10;
+    return secondDiget;
+}
+//Тело программы
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (CheckThreeDiget(number)) Console.WriteLine($"Вторая цифра числа {number} => {CheckSecondDiget(number)}");
+else Console.WriteLine("число не трёхзначное");
