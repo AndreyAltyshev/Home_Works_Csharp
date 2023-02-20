8e9bc33380f3cdbc6baa383ca04d1e9a56092355
Norros <Andriyxa_77@mail.ru>
2023-02-20 21:34:57 +0300

    HomeWork_3

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,19 +4,24 @@
 14212 -> нет
 12821 -> да
 23432 -> да */
+
 // Тело программы
 int number = Prompt("Введите число");       //ввод данных
+
 int revNumber = ReverseNum(number);         //получаем развёрнутое число
+
 if (number == revNumber) Console.WriteLine($"Число {number} является палиндромом");         //сравниваем на палиндром
 else Console.WriteLine($"Число {number} не является палиндромом");
 
 //Методы
+
 int Prompt(string mess)                                           //метод ввода данных
 {
     Console.WriteLine(mess);
     int num = Convert.ToInt32(Console.ReadLine());
     return num;
 }
+
 int ReverseNum(int num)                                        //метод разворачивающий число
 {
     int revNum = 0;
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,7 +1,6 @@
 ﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
 A (3,6,8); B (2,1,-7), -> 15.84
-
 A (7,-5, 0); B (1,-1,9) -> 11.53 */
 
 // Тело программы
@@ -20,12 +19,14 @@ double result = Math.Sqrt((Squ(xb-xa)+Squ(yb-ya)+Squ(zb-za)));  //вычесля
 Console.WriteLine($"Расстояние между точками равно {Math.Round(result, 2, MidpointRounding.ToZero)}");  
 
 //методы
+
 int Prompt(string mess)                 //метод ввода данных
 {
     Console.Write(mess);
     int num = Convert.ToInt32(Console.ReadLine());
     return num;
 }
+
 int Squ(int num)                        //метод возводящий число в квадрат
 {
     return num*num;
